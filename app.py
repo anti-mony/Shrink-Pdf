8bd0a01b3f7d17085b5192c1b3053553b0574426
Sushant Bansal <sushantbansal2@gmail.com>
2019-06-23 17:46:29 -0500

    Updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
-import schedule
+from datetime import datetime
+import hashlib
 from flask import Flask, render_template, request, jsonify, send_file, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
@@ -8,8 +9,17 @@ app = Flask(__name__)
 UPLOAD_FOLDER = './Uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 resolution = ''
+
+from rq import Queue
+from worker import conn
+import uitl
+
+q = Queue(connection=conn)
+
+
 @app.route("/")
 def main():
+    q.enqueue(clean)
     return render_template('index.html')
 
 @app.route("/error")
@@ -21,24 +31,22 @@ def errorPage():
 def shrinkFile():
     f = request.files['pdffile']
     resolution = request.form['dpi']
+    timeHash = hashlib.md5(str(datetime.now()).encode()).hexdigest()
     # print(f.filename)
-    fname = secure_filename(f.filename)
+    fname = timeHash + "_" + secure_filename(f.filename)
     f.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
     try:
-        subprocess.check_call(['./Uploads/shrinkpdf.sh','./Uploads/'+fname,"./Uploads/Small_"+resolution+"_"+fname,resolution])
+        subprocess.check_call(['./Uploads/shrinkpdf.sh','./Uploads/'+fname,"./Uploads/Shrinked_"+resolution+"_"+fname,resolution])
     except subprocess.CalledProcessError:
         errorPage()
     # return url_for(errorPage), 500
-    return jsonify(fileURL=url_for('shrinkedFile',filename=fname),
-                    cfs=int(os.path.getsize("./Uploads/Small_"+fname)))
+    return jsonify(fileURL=url_for('shrinkedFile',filename='Shrinked_'+resolution+"_"+fname),
+                    cfs=int(os.path.getsize("./Uploads/Shrinked_"+resolution+"_"+fname)))
  
 @app.route('/Uploads/<filename>')
 def shrinkedFile(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], 'Small_'+resolution+"_"+filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-def deleteFiles():
-    subprocess.call(['./Uploads/rm -rf *.pdf'])
-    schedule.every().hour.do(job)
 
 if __name__ == "__main__":
     app.run()
